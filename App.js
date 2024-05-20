19c9971dff62741858389d880dcdb694f4edad50
LAHSIVA <vishalvenkat2019@gmail.com>
2024-05-21 00:46:22 +0530

    feat : created Applayout and Header

--- App.js
+++ App.js
@@ -2,16 +2,34 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 
+
+const Header =() =>{
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo9T_b7XTjvXI85fTQKSWFfcKHuJLi2oGClw&s"/>
+            </div>
+            <div className="nav-items">
+                <ul className="nav-list">
+                    <li className="nav-Home">Home</li>
+                    <li className="nav-About">About</li>
+                    <li className="nav-Contact">Contact</li>
+                    <li className="nav-Cart">Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
 const AppLayout = () => {
     return(
-        <div class="app">
-            
-
+        <div className="app">
+            <Header />
         </div>
-    )
+    );
     
 };
 
 const root =ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Header/>);
+root.render(<AppLayout/>);
